d4e5b6410f022e3a48cb69c329fde3936db81812
Gitanjali <83583240+gitssz@users.noreply.github.com>
2022-11-17 13:12:30 +0530

    Add files via upload

---
+++ Pass1/obj.java
@@ -0,0 +1,11 @@
+package pattern;
+
+public class obj {
+	String name;
+	int addr;
+
+	obj(String nm, int address) {
+		this.name = nm;
+		this.addr = address;
+	}
+}
---
+++ Pass1/pooltable.java
@@ -0,0 +1,11 @@
+package pattern;
+
+public class pooltable {
+	int first, total_literals;
+
+	public pooltable(int f, int l) {
+// TODO Auto-generated constructor stub
+		this.first = f;
+		this.total_literals = l;
+	}
+}
