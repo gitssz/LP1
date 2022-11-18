5a562b66ab8a0473a3039f3a94a8d79d10109a08
Gitanjali <83583240+gitssz@users.noreply.github.com>
2022-11-18 14:36:41 +0530

    update

--- Banker.java
+++ Banker.java
@@ -1,14 +1,12 @@
-package pattern;
+package spos;
 import java.util.*;
 public class Banker {
     public static void main(String[] args)
     {
-        int processes[] = {0, 1, 2, 3, 4};
-
+    
         int avail[] = {3,1,1,2};
 
-
-        int maxm[][] = {{3,3,2,2},
+        int max[][] = {{3,3,2,2},
                 {1,1,3,4},
                 {1,3,5,0}};
 
@@ -17,22 +15,22 @@ public class Banker {
                 {1,0,3,3},
                 {1,2,1,0}};
 
-        isSafe(processes, avail, maxm, allot);
+        isSafe( avail, max, allot);
     }
         static int P = 3;
         static int R = 4;
-        static void calculateNeed(int need[][], int maxm[][], int allot[][])
+        static void calculateNeed(int need[][], int max[][], int allot[][])
         {
             for (int i = 0 ; i < P ; i++)
                 for (int j = 0 ; j < R ; j++)
 
-                    need[i][j] = maxm[i][j] - allot[i][j];
+                    need[i][j] = max[i][j] - allot[i][j];
         }
-        static boolean isSafe(int processes[], int avail[], int maxm[][],
+        static boolean isSafe( int avail[], int max[][],
                               int allot[][])
         {
             int [][]need = new int[P][R];
-            calculateNeed(need, maxm, allot);
+            calculateNeed(need, max, allot);
             boolean []finish = new boolean[P];
             int []safeSeq = new int[P];
             int []work = new int[R];
@@ -66,9 +64,37 @@ public class Banker {
                     }
                 }
 
+
+                System.out.println("availble matrix: ");
+                for(int i=0;i<avail.length;i++) {
+                	System.out.print(avail[i]+" ");
+                }
+                System.out.println("\n\nmax matrix: ");
+                for(int i=0;i<P;i++) {
+                	for(int j=0;j<R;j++) {
+                		System.out.print(max[i][j]+ " ");
+                	}
+                	System.out.println();
+                }
+                System.out.println("\nneed matrix: ");
+                for(int i=0;i<P;i++) {
+                	for(int j=0;j<R;j++) {
+                		System.out.print(need[i][j]+ " ");
+                	}
+                	System.out.println();
+                }
+                System.out.println("\nallocation matrix: ");
+                for(int i=0;i<P;i++) {
+                	for(int j=0;j<R;j++) {
+                		System.out.print(allot[i][j]+ " ");
+                	}
+                	System.out.println();
+                }
+                
+                
                 if (found == false)
                 {
-                    System.out.print("System is not in safe state");
+                    System.out.print("\nSystem is not in safe state");
                     return false;
                 }
             }
