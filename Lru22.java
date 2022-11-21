2d17cb2c71517d6fee61ff57f340af65b45e28f9
Gitanjali <83583240+gitssz@users.noreply.github.com>
2022-11-21 15:30:50 +0530

    Update Lru22.java

--- Lru22.java
+++ Lru22.java
@@ -1,42 +1,73 @@
-package spos;
-import java.util.ArrayList;
-public class Lru {
-	
-
-	public static void main(String[] args) {
-		int capacity = 3;
-		int arr[] = {2 ,3 ,2 ,1 ,5 ,2 ,4 ,5 ,3 ,2 ,5 ,2 };
-		
-		
-		ArrayList<Integer> s=new ArrayList<>(capacity);
-		int count=0;
-		int page_faults=0;
-		for(int i:arr)
-		{
-			// Insert it into set if not present
-			if(!s.contains(i))
-			{
-			
-			// Check if the set can hold equal pages
-			if(s.size()==capacity)
-			{
-				s.remove(0);
-				s.add(capacity-1,i);
-			}
-			else
-				s.add(count,i);
-				page_faults++;
-				++count;
-		
-			}
-			else
-			{
-
-				s.remove((Object)i);
-				s.add(s.size(),i);		
-			}
-		
-		}
-		System.out.println("page faults: " + page_faults);
-	}
+package ;
+import java.io.*;
+import java.util.*;
+public class lru{
+ public static void main(String[] args) throws IOException {
+ BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ int frames, pointer = 0, hit = 0, fault = 0, ref_len;
+ Boolean isFull = false;
+ int buffer[];
+ ArrayList<Integer> stack = new ArrayList<Integer>();
+ int reference[];
+ int mem_layout[][];
+ System.out.println("Please enter the number of Frames: ");
+ frames = Integer.parseInt(br.readLine());
+ System.out.println("Please enter the length of the Reference string: ");
+ ref_len = Integer.parseInt(br.readLine());
+ reference = new int[ref_len];
+ mem_layout = new int[ref_len][frames];
+ buffer = new int[frames];
+ for (int j = 0; j < frames; j++)
+ buffer[j] = -1;
+ System.out.println("Please enter the reference string: ");
+ for (int i = 0; i < ref_len; i++) {
+ reference[i] = Integer.parseInt(br.readLine());
+ }
+ System.out.println();
+ for (int i = 0; i < ref_len; i++) {
+ if (stack.contains(reference[i])) {
+ stack.remove(stack.indexOf(reference[i]));
+ }
+ stack.add(reference[i]);
+ int search = -1;
+ for (int j = 0; j < frames; j++) {
+ if (buffer[j] == reference[i]) {
+ search = j;
+ hit++;
+ break;
+ }
+ }
+ if (search == -1) {
+ if (isFull) {
+ int min_loc = ref_len;
+ for (int j = 0; j < frames; j++) {
+ if (stack.contains(buffer[j])) {
+ int temp = stack.indexOf(buffer[j]);
+ if (temp < min_loc) {
+ min_loc = temp;
+ pointer = j;
+ }
+ }
+ }
+ }
+ buffer[pointer] = reference[i];
+ fault++;
+ pointer++;
+ if (pointer == frames) {
+ pointer = 0;
+ isFull = true;
+ }
+ }
+ for (int j = 0; j < frames; j++)
+ mem_layout[i][j] = buffer[j];
+ }
+ for (int i = 0; i < frames; i++) {
+ for (int j = 0; j < ref_len; j++)
+ System.out.printf("%3d ", mem_layout[j][i]);
+ System.out.println();
+ }
+ System.out.println("The number of Hits: " + hit);
+ System.out.println("Hit Ratio: " + (float) ((float) hit / ref_len));
+ System.out.println("The number of Faults: " + fault);
+ }
 }
